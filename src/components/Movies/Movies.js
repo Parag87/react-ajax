084e47ba8fad97cab6163b39f7b46da787d07c47
Anusha <apaka@allstate.com>
2017-01-10 15:35:15 +0530

    React project movie work

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -4,12 +4,26 @@ import axios from 'axios';
 export default class Movies extends React.Component{
   constructor(props){
       super(props);
-      this.state = {};
+      this.state = {results: []};
       this.search = this.search.bind(this);
+      this.clear = this.clear.bind(this);
   }
 
   search(){
-    const num = +this.search.value;
+    const query = this.query.value;
+    const url =`http://www.omdbapi.com/?s=${query}&page=1`;
+    axios.get(url)
+      .then(response => {
+        const results = response.data.Search;
+        this.setState({results});
+      })
+      .catch(error => {
+        console.log(error);
+    });
+  }
+  clear(){
+    this.query.value = "";
+    this.setState({results: []});
   }
 
   render(){
@@ -19,9 +33,35 @@ export default class Movies extends React.Component{
           <div className = "panel panel-default">
             <div className = "panel-body">
               <label>Search</label>
-              <input ref={n => this.query =n} type="number" />
-              <button onClick={this.search} className="btn btn-primary btn-info">Search</button>
+              <input ref={n => this.query =n} type="text" />
+              <button onClick={this.search} className="btn btn-primary btn-sm">Search</button>
+              <button onClick={this.clear} className="btn btn-danger btn-sm">Clear</button>
+
             </div>
+          </div>
+          <div>
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Year</th>
+                <th>Poster</th>
+              </tr>
+            </thead>
+            <tbody>
+            {
+              this.state.results.map((r,i) => {
+                return(
+                  <tr key={i}>
+                    <td>{r.Title}</td>
+                    <td>{r.Year}</td>
+                    <td><img src={r.Poster} /></td>
+                  </tr>
+                );
+              })
+            }
+            </tbody>
+          </table>
           </div>
         </div>
     );
